ebb10db1e44f80546c32d189760ba657812ec103
redfox193 <cer2005dima@gmail.com>
2023-09-27 18:33:51 +0300

    Create FastAPI app with health/ endpoint

---
+++ app/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import RedirectResponse, JSONResponse
+from starlette import status
+import uvicorn
+
+app = FastAPI()
+
+
+@app.get("/")
+def root(request: Request):
+    return RedirectResponse(request.url.url + "health")
+
+
+@app.get("/health")
+def health_endpoint():
+    return JSONResponse(status_code=status.HTTP_200_OK, content={})
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
