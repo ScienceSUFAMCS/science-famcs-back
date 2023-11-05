8f91b4d072f95938eea3248a6a59dafa8ff2d189
redfox193 <cer2005dima@gmail.com>
2023-11-05 13:42:30 +0300

    test: add test to health_check endpoint

--- tests/test_classes/test_sample.py
+++ tests/test_classes/test_sample.py
@@ -1,6 +1,20 @@
+import os
+import sys
 import pytest
+from fastapi.testclient import TestClient
+
+test_dir = os.path.dirname(__file__)
+module_dir = os.path.join(test_dir, "../..")
+sys.path.append(module_dir)
+
+from app.main import app
+
+
+client = TestClient(app)
 
 
 @pytest.mark.asyncio
-async def test_sample():
-    assert 1 == 1
+async def test_health_endpoint():
+    response = client.get("/health")
+    assert response.status_code == 200
+    assert response.json() == {}
